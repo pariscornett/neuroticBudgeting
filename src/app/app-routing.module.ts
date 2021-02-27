6d925bb34147a0325b244faaa99b7c1f6940b2b0
Paris Cornett <pariscornett@gmail.com>
2021-02-26 23:42:40 -0600

    adds home component and alphabetizes imports, route objects, etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AddGoalComponent } from './add-goal/add-goal.component';
 import { BudgetOverviewComponent } from './budget-overview/budget-overview.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { SettingsComponent } from './settings/settings.component';
 
-//sets up routes constant where you define your routes
+//define routes
 const routes: Routes = [
-{path: 'dashboard', component: DashboardComponent},
-{path: 'settings', component: SettingsComponent},
-{path: 'registration', component: RegistrationComponent},
-{path: 'budget-overview', component: BudgetOverviewComponent},
-{path: 'add-goal', component: AddGoalComponent}
+  {path: 'add-goal', component: AddGoalComponent},
+  {path: 'budget-overview', component: BudgetOverviewComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', component: HomeComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'settings', component: SettingsComponent}
 ];
 
 //configures ngModule imports and exports
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AddGoalComponent } from './add-goal/add-goal.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SettingsComponent } from './settings/settings.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { BudgetOverviewComponent } from './budget-overview/budget-overview.component';
-import { AddGoalComponent } from './add-goal/add-goal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
+    AddGoalComponent,
     AppComponent,
-    DashboardComponent,
-    SettingsComponent,
-    RegistrationComponent,
     BudgetOverviewComponent,
-    AddGoalComponent
+    DashboardComponent,
+    HomeComponent,
+    RegistrationComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
